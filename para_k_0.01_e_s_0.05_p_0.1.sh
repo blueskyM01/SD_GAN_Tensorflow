c695e79afb8b2919b6787d93daff137fc87d8bd9
Jianbing Yang <1226853967@qq.com>
2019-02-27 21:17:54 +0800

    with pose expression shape preserving change lambda_k=0.01

--- para_k_0.01_e_s_0.05_p_0.1.sh
+++ para_k_0.01_e_s_0.05_p_0.1.sh
@@ -1,11 +1,11 @@
 python run_this.py \
---gpu_assign='1' \
+--gpu_assign='0' \
 --is_train=True \
 --log_dir='/media/yang/F/ubuntu/SD_GAN_Result/with pose shape expression lambdak=0.001/logs' \
 --sampel_save_dir='/media/yang/F/ubuntu/SD_GAN_Result/with pose shape expression lambdak=0.001/samples' \
 --checkpoint_dir='/media/yang/F/ubuntu/SD_GAN_Result/with pose shape expression lambdak=0.001/checkpoint' \
 --test_sample_save_dir='/media/yang/F/ubuntu/SD_GAN_Result/with pose shape expression lambdak=0.001/test_sample' \
---lambda_k=0.001 \
+--lambda_k=0.01 \
 --lambda_s=0.05 \
 --lambda_e=0.05 \
 --lambda_p=0.1 \
